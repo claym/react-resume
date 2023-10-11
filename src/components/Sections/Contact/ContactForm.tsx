9377fb98bea78e5fb0440f10b160eadc4d1149dc
Clay Mitchell <clay@pfd.net>
2023-10-10 20:15:32 -0400

    disable contact submit button until it actually works

--- src/components/Sections/Contact/ContactForm.tsx
+++ src/components/Sections/Contact/ContactForm.tsx
@@ -67,6 +67,7 @@ const ContactForm: FC = memo(() => {
       <button
         aria-label="Submit contact form"
         className="w-max rounded-full border-2 border-sky-600 bg-slate-900 px-4 py-2 text-sm font-medium text-white shadow-md outline-none hover:bg-stone-800 focus:ring-2 focus:ring-sky-600 focus:ring-offset-2 focus:ring-offset-slate-800"
+        disabled={true}
         type="submit">
         Send Message
       </button>
